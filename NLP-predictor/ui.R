ba6074fd3fc485118b8cdc134cd2d19561ea4a0a
MadBluey <karlkasparhaavel@gmail.com>
2017-11-18 10:48:12 +0200

    UI creation

---
+++ NLP-predictor/ui.R
@@ -0,0 +1,33 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+# 
+#    http://shiny.rstudio.com/
+#
+
+library(shiny)
+
+# Define UI for application that draws a histogram
+shinyUI(fluidPage(
+    
+    # Application title
+    titlePanel("Natural Language Processing - Next Word Predictor."),
+    
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(position = "left",
+                     textInput("text1", label = "Insert sentence here:"), 
+                     actionButton("update","Submit")
+        ),
+        
+        # Show a plot of the generated distribution
+        mainPanel("main panel",
+                  fluidRow(splitLayout(cellWidths = c("50%","50%")),
+                           plotOutput("plot1"),plotOutput("plot2")
+                  )
+        )
+    )
+)
+)
